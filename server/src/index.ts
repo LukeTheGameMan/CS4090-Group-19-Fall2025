9f9884571377115e928d7b183f7b6136017e5bdc
LukeTheGameMan <lukesinclair200@gmail.com>
2025-11-15 20:33:42 -0600

    Removed login.html, modified index.html, index.ts, added main.html, signup.html

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,26 @@
 import express, { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
 const app = express();
 const prisma = new PrismaClient();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../../web_dir')));
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('this is so poggers');
+});
+
+app.get('/login', (req: Request, res: Response) => {
+    res.redirect('main.html');
+});
+
+app.get('/guest', (req: Request, res: Response) => { 
+    res.redirect('main.html');
 });
 
 app.listen(port, () => {
